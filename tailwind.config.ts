a8ea4f76dac91073b943f5be11385d627217f106
aan <aanwidianto01@gmail.com>
2025-01-27 16:58:35 +0700

    setting color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,53 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        md: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        light: {
+          bg: "#f4f4f5",
+          'bg-2': "#e2e8f0",
+          fg: "#0f172a",
+          'fg-2': "#334155",
+          primary: "#0284c7",
+        },
+
+        dark: {
+          bg: "#0f172a",
+          'bg-2': "#1e293b",
+          fg: "#e2e8f0",
+          'fg-2': "#f4f4f5",
+          primary: "#1e40af",
+        },
+
+        info: "#0284c7",
+        success: "#10b981",
+        warning: "#f59e0b",
+        error: "#dc2626",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+      },
+      fontFamily: {
+        sans: ["Montserrat", "Poppins", "sans-serif"],
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;
